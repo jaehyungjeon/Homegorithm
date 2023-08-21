f8031591d64d7e8056518ee166dc3de8d539a66b
jaehyungjeon <jaehyung601@naver.com>
2023-08-21 14:59:54 +0900

    commit

--- Intensification/YourScore.java
+++ Intensification/YourScore.java
@@ -67,7 +67,7 @@ public class YourScore {
 		double sum = 0.0;
 		for(int i=0; i<20; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			String course = st.nextToken();
+			st.nextToken();
 			double score = Double.parseDouble(st.nextToken());
 			String grade = st.nextToken();
 			
