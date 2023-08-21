b0e4ef24c71080a1d516787cb719898cac8f1c72
jaehyungjeon <jaehyung601@naver.com>
2023-08-21 13:08:33 +0900

    commit

---
+++ Intensification/Sprout.java
@@ -0,0 +1,31 @@
+package Intensification;
+
+/*
+	Question : 아래 예제와 같이 새싹을 출력하시오.
+	
+	input			 
+	
+	output
+		         ,r'"7
+	r`-_   ,'  ,/
+	 \. ". L_r'
+	   `~\/
+	      |
+	      |
+	
+	Solution : 1. 그냥 프린트..
+
+*/
+
+public class Sprout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+	}
+}
