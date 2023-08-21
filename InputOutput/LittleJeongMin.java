65bc9bf493c9816795f5db88d057175e4613f8fc
jaehyungjeon <jaehyung601@naver.com>
2023-08-21 09:30:45 +0900

    commit

---
+++ InputOutput/LittleJeongMin.java
@@ -0,0 +1,38 @@
+package InputOutput;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+	Question : 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+	
+	input			 
+	77 77 7777
+	
+	output
+	7931
+	
+	Solution : 1. 입력 받은 수 계산
+
+*/
+
+
+public class LittleJeongMin {
+
+	public static long answer = 0;
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		
+		answer = A+B+C;
+		System.out.println(answer);
+	}
+
+}
