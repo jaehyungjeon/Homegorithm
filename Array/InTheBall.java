643521df97cf3f08f22b6006b2b01f7e32a75802
jaehyungjeon <jaehyung601@naver.com>
2023-08-21 10:23:41 +0900

    commit

--- Array/InTheBall.java
+++ Array/InTheBall.java
@@ -5,26 +5,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /*
-	Question : 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다. 또, 1번부터 N번까지 번호가 적혀있는 공을 매우 많이 가지고 있다. 
-	가장 처음 바구니에는 공이 들어있지 않으며, 바구니에는 공을 1개만 넣을 수 있다.
+	Question : 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다. 
+	바구니에는 공이 1개씩 들어있고, 처음에는 바구니에 적혀있는 번호와 같은 번호가 적힌 공이 들어있다.
 	
-	도현이는 앞으로 M번 공을 넣으려고 한다. 도현이는 한 번 공을 넣을 때, 공을 넣을 바구니 범위를 정하고, 정한 바구니에 모두 같은 번호가 적혀있는 공을 넣는다. 
-	만약, 바구니에 공이 이미 있는 경우에는 들어있는 공을 빼고, 새로 공을 넣는다. 공을 넣을 바구니는 연속되어 있어야 한다.
+	도현이는 앞으로 M번 공을 바꾸려고 한다. 도현이는 공을 바꿀 바구니 2개를 선택하고, 두 바구니에 들어있는 공을 서로 교환한다.
 	
-	공을 어떻게 넣을지가 주어졌을 때, M번 공을 넣은 이후에 각 바구니에 어떤 공이 들어 있는지 구하는 프로그램을 작성하시오.
+	공을 어떻게 바꿀지가 주어졌을 때, M번 공을 바꾼 이후에 각 바구니에 어떤 공이 들어있는지 구하는 프로그램을 작성하시오.
 	
 	input			 
 	5 4
-	1 2 3
-	3 4 4
-	1 4 1
-	2 2 2
+	1 2
+	3 4
+	1 4
+	2 2
 	
 	output
-	1 2 1 1 0
+	3 1 4 2 5
 	
-	Solution : 1. 반복문을 사용햐여 배열의 값 새롭게 input
-			   2. 내용 자체는 어려움 없음. 없을때 배열에 0 한번 넣어주는게 포인트
+	Solution : 1. for문을 기반으로 최초 배열에 번호로 초기화 실시
+	 		   2. 임시 temp에 값을 넣고 배열 값 교환 실시
+	 		   3. 마지막 전까지 공백을 builder에 추가.
+	 		   4. 값 반환
 
 */
 
@@ -43,31 +44,34 @@ public class InTheBall {
 		
 		arr = new int[N];
 		
+		// 배열에 값 초기화 한번 실시
+		for(int i=0; i<N; i++) {
+			arr[i] = i+1;
+		}
+		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int start =  Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int countNum = Integer.parseInt(st.nextToken());
+			int arrival = Integer.parseInt(st.nextToken());
 			
-			arrMethod(start, end, countNum);
+			arrMethod(start, arrival);
 		}
 		
 		for(int i=0; i<N; i++) {
-			if(arr[i] == 0) {
-				arr[i] = 0;
+			sb.append(arr[i]);
+			if(i != N-1) {
+				sb.append(" ");
 			}
-			
-			sb.append(arr[i] + " ");
 		}
-		
 		System.out.println(sb.toString());
 	}
 	
-	private static void arrMethod(int start, int end, int countNum) {
-		for(int i=start-1; i<end; i++) {
-			arr[i] = countNum;
-		}
+	// 단순 배열 Exchange Method
+	private static void arrMethod(int start, int arrival) {
+		int temp = arr[arrival-1];
+		arr[arrival-1] = arr[start-1];
+		arr[start-1] = temp;
 	}
 
 }
