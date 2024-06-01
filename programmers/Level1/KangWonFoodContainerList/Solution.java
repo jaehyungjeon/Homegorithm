ab3aeedf9b4dbfea9c985010d51c359bb2bd9048
jaehyungjeon <jaehyung601@naver.com>
2024-06-01 21:57:21 +0900

    commit

---
+++ programmers/Level1/KangWonFoodContainerList/Solution.java
@@ -0,0 +1,14 @@
+package programmers.Level1.KangWonFoodContainerList;
+
+/*
+	Question : FOOD_FACTORY 테이블에서 강원도에 위치한 식품공장의 공장 ID, 공장 이름, 주소를 조회하는 SQL문을 작성해주세요. 이때 결과는 공장 ID를 기준으로 오름차순 정렬해주세요.
+	
+	Solution : 1. 강원도 창고 기준에 맞추어 출력
+*/
+
+public class Solution {
+	SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+	FROM FOOD_FACTORY
+	WHERE ADDRESS LIKE '%강원도%'
+	ORDER BY FACTORY_ID
+}
